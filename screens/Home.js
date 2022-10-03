6860b111f7f4c9f892aebe89f563a392d9e59c51
Jahan Zeb Ahmed <jjahanzebb@gmail.com>
2022-10-02 23:21:53 -0700

    day 3.0 - Added todo completed check, new home screen design

--- screens/Home.js
+++ screens/Home.js
@@ -7,6 +7,7 @@ import {
   TouchableOpacity,
   Keyboard,
   Pressable,
+  StatusBar,
 } from "react-native";
 import React, { useState, useEffect } from "react";
 import { firebase } from "../config";
@@ -29,12 +30,31 @@ const Home = () => {
   const [isChecked, setChecked] = useState([]);
 
   const handleChecked = (item) => {
-    if (isChecked.indexOf(item.id) >= 0) {
-      isChecked.splice(isChecked.indexOf(item.id), 1);
-    } else {
-      isChecked.push(item.id);
-    }
-    console.log(isChecked);
+    // if (isChecked.indexOf(item.id) >= 0) {
+    //   isChecked.splice(isChecked.indexOf(item.id), 1);
+    // } else {
+    //   isChecked.push(item.id);
+    // }
+    // console.log(isChecked);
+
+    todoRef
+      .doc(item.id)
+      .update({
+        completed: !item.completed,
+      })
+      .then(() => {
+        console.log("Status Updated!");
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  };
+
+  const generateColor = () => {
+    const randomColor = Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, "0");
+    return `#${randomColor}`;
   };
 
   // fetch/read todo from firebase database
@@ -43,9 +63,13 @@ const Home = () => {
       const todos = [];
       querySnapshot.forEach((doc) => {
         const { heading } = doc.data();
+        const { completed } = doc.data();
+        const { color } = doc.data();
         todos.push({
           id: doc.id,
           heading,
+          completed,
+          color,
         });
       });
       setTodos(todos);
@@ -76,6 +100,8 @@ const Home = () => {
       const data = {
         heading: addData,
         createdAt: timestamp,
+        completed: false,
+        color: generateColor(),
       };
 
       todoRef
@@ -92,99 +118,147 @@ const Home = () => {
   };
 
   return (
-    <SafeAreaView style={[tw`flex-1`, { backgroundColor: "#F1FCFE" }]}>
-      {/* formContainer */}
-      <View style={tw`flex-row h-12 mx-4 mt-24 rounded-xl `}>
-        {/* input */}
-        <TextInput
-          style={[
-            tw`h-12 rounded-l-xl px-4 pr-2 text-base overflow-hidden flex-1`,
-            { backgroundColor: "#1D3557", color: "#F1FCFE" },
-          ]}
-          placeholder="Enter a new Task to do.."
-          placeholderTextColor="#888899"
-          onChangeText={(heading) => setAddData(heading)}
-          value={addData}
-          underlineColorAndroid="transparent"
-          autoCapitalize="none"
-        />
+    <SafeAreaView style={[tw`flex-1`, { backgroundColor: "#1D3557" }]}>
+      <StatusBar barStyle={"light-content"} backgroundColor={"#1D3557"} />
 
-        {/* button */}
-        <TouchableOpacity
-          style={[
-            tw`h-12 rounded-r-xl px-4.5 items-center justify-center`,
-            { backgroundColor: "#3890C7" },
-          ]}
-          onPress={addTodo}
-        >
-          {/* buttonText */}
-          {/* <Text style={[tw`text-base`, { color: "#F1FCFE" }]}>Add</Text> */}
+      {/* white container */}
+      <View
+        style={[tw`flex-1 rounded-tl-full`, { backgroundColor: "#F1FCFE" }]}
+      >
+        {/* Logo */}
+        <View style={[tw`justify-start flex-row mt-8 mx-4 rounded-lg`]}>
+          <Text
+            style={[
+              tw`text-2xl font-800 rounded-l-lg pl-2 pr-1.5 py-0.5`,
+              { color: "#3890C7", backgroundColor: "#F1FCFE" },
+            ]}
+          >
+            My
+          </Text>
 
-          <Entypo
-            style={[tw`text-xl`, { elevation: 5 }]}
-            name="plus"
-            color="#F1FCFE"
+          <Text
+            style={[
+              tw`text-2xl font-800 rounded-r-lg pr-2 pl-1.5 py-0.5`,
+              { color: "#F1FCFE", backgroundColor: "#3890C7" },
+            ]}
+          >
+            to-dos
+          </Text>
+        </View>
+
+        {/* formContainer */}
+        <View style={[tw`flex-row h-12 mx-4 mt-14`, { elevation: 10 }]}>
+          {/* input */}
+          <TextInput
+            style={[
+              tw`h-12 rounded-l-xl px-4 pr-2 text-base overflow-hidden flex-1`,
+              { backgroundColor: "#1D3557", color: "#F1FCFE", elevation: 5 },
+            ]}
+            placeholder="Enter a new to-do.."
+            placeholderTextColor="#888899"
+            onChangeText={(heading) => setAddData(heading)}
+            value={addData}
+            underlineColorAndroid="transparent"
+            autoCapitalize="none"
           />
-        </TouchableOpacity>
-      </View>
 
-      <FlatList
-        style={tw`mt-4`}
-        data={todos}
-        keyExtractor={(item) => item.id}
-        numColumns={1}
-        renderItem={({ item }) => (
-          <View>
-            {/* container */}
-            <TouchableOpacity
-              style={[
-                tw`h-12 px-4 mt-2 mr-4 flex-row items-center rounded-r-xl `,
-                { backgroundColor: "#1D3557", elevation: 5 },
-              ]}
-              onPress={() => navigation.navigate("Details", { item })}
-            >
-              <TouchableOpacity
-                style={tw`rounded-l-xl w-12 h-12 bg-gray-600 ${
-                  !isChecked.indexOf(item.id) >= 0 && "bg-gray-500"
-                }`}
-                onPress={() => handleChecked(item)}
-              />
+          {/* button */}
+          <TouchableOpacity
+            style={[
+              tw`h-12 rounded-r-xl px-4.5 items-center justify-center`,
+              { backgroundColor: "#3890C7", elevation: 10 },
+            ]}
+            onPress={addTodo}
+            activeOpacity={0.5}
+          >
+            {/* buttonText */}
+            {/* <Text style={[tw`text-base`, { color: "#F1FCFE" }]}>Add</Text> */}
 
-              {/* innerContainer */}
-              <View style={tw`items-center flex-row ml-3 flex-1`}>
-                {/* itemHeading */}
-                <Text
-                  style={[
-                    tw`text-base font-bold mr-5 ${
-                      !isChecked.indexOf(item.id) >= 0 && "line-through"
-                    }`,
-                    { color: "#F1FCFE" },
-                  ]}
-                >
-                  {item.heading[0].toUpperCase() + item.heading.slice(1)}
-                </Text>
-              </View>
+            <Entypo
+              style={[tw`text-xl`, { elevation: 5 }]}
+              name="plus"
+              color="#F1FCFE"
+            />
+          </TouchableOpacity>
+        </View>
 
-              {/* todoIcon */}
+        <FlatList
+          style={tw`mt-4`}
+          data={todos}
+          keyExtractor={(item) => item.id}
+          numColumns={1}
+          renderItem={({ item }) => (
+            <View>
+              {/* container */}
               <TouchableOpacity
                 style={[
-                  tw`text-2xl -mr-4 py-2.5 px-4.5 rounded-r-xl`,
-                  {
-                    backgroundColor: "#E63946",
-                  },
+                  tw`h-12 pr-4 mt-2 mx-4 flex-row items-center rounded-xl `,
+                  { backgroundColor: "#1D3557", elevation: 5 },
                 ]}
-                onPress={() => deleteTodo(item)}
+                onPress={() => navigation.navigate("Details", { item })}
+                activeOpacity={0.85}
               >
-                <MaterialCommunityIcons
-                  style={[tw`text-xl`, { elevation: 5 }]}
-                  name="delete-outline"
-                  color="#F1FCFE"
+                <View
+                  style={[
+                    tw`w-2.5 h-12 rounded-l-xl`,
+                    {
+                      backgroundColor: item.color,
+                    },
+                  ]}
                 />
+
+                <TouchableOpacity
+                  style={tw`ml-2 rounded-full w-8 h-8 border-2 border-white items-center justify-center  ${
+                    item.completed && "bg-emerald-500"
+                  }`}
+                  onPress={() => handleChecked(item)}
+                >
+                  {item.completed === true && (
+                    <MaterialCommunityIcons
+                      style={[tw`text-base`, { elevation: 2 }]}
+                      name="check-bold"
+                      color="#F1FCFE"
+                    />
+                  )}
+                </TouchableOpacity>
+
+                {/* innerContainer */}
+                <View style={tw`items-center flex-row ml-3 flex-1`}>
+                  {/* itemHeading */}
+                  <Text
+                    style={[
+                      { color: "#F1FCFE" },
+                      tw`text-base font-bold mr-5 ${
+                        item.completed &&
+                        "line-through text-slate-400 font-semibold"
+                      }`,
+                    ]}
+                  >
+                    {item.heading[0].toUpperCase() + item.heading.slice(1)}
+                  </Text>
+                </View>
+
+                {/* todoIcon */}
+                <TouchableOpacity
+                  style={[
+                    tw`text-2xl -mr-4 py-2.5 px-4.5 rounded-r-xl`,
+                    {
+                      backgroundColor: "#E63946",
+                    },
+                  ]}
+                  onPress={() => deleteTodo(item)}
+                >
+                  <MaterialCommunityIcons
+                    style={[tw`text-xl`, { elevation: 5 }]}
+                    name="delete-outline"
+                    color="#F1FCFE"
+                  />
+                </TouchableOpacity>
               </TouchableOpacity>
-            </TouchableOpacity>
-          </View>
-        )}
-      />
+            </View>
+          )}
+        />
+      </View>
     </SafeAreaView>
   );
 };
