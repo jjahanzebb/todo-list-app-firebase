219088ede6f9fbae31b296b5e810461b4d541ee2
Jahan Zeb Ahmed <jjahanzebb@gmail.com>
2022-10-04 11:49:03 +0500

    day 4.0 - Redesigned Details Screen

---
+++ config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+import { firebaseConfig } from "./firebaseConfig";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+// import firebaseConfig.js //
+
+// Initialize Firebase
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+export { firebase };
